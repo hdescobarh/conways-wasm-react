92201342d0fc9013c36baf72a65ca5ee3994b657
Hans Escobar <hdescobarh@unal.edu.co>
2023-01-14 01:31:48 -0500

    Setting the VS Code and enviroment configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 // https://tailwindcss.com/docs/installation/using-postcss
+// https://github.com/tailwindlabs/prettier-plugin-tailwindcss#resolving-your-tailwind-configuration
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-    './public/index.html'],
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
